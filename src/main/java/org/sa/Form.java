391fc14b53fcb73ab22ecd0df8b6605fbe066724
wishpath <117854313+wishpath@users.noreply.github.com>
2024-08-05 23:34:28 +0300

    update from bitbucket 'form'

--- src/main/java/org/sa/Form.java
+++ src/main/java/org/sa/Form.java
@@ -2,18 +2,35 @@ package org.sa;
 
 import java.util.stream.Stream;
 
+/**
+ * Superior to String.format(CONSTANT, additive1, additive2) for better readable usage:
+ * CONSTANT.with(additive1, additive1);
+ * <p>
+ * Superior to String CONSTANT = "example %s abc" for having described each insertable:
+ * Form CONSTANT = new Form("example %index abc");
+ **/
 public class Form {
+  private static final String WORD_EDGE = "(?<=\\S)(?=\\s)|(?<=\\s)(?=\\S)";
   private StringBuilder format = new StringBuilder();
 
+  /**
+   * new Form("example", "%index", "abc") results in "example%sabc"
+   **/
   public Form(String... formAdditives) {
     Stream.of(formAdditives)
-        .forEach(part -> format.append(part.startsWith("%") ? "%s" : part));
+          .forEach(additive -> format.append(additive.startsWith("%") ? "%s" : additive));
+  }
+
+  /**
+   * new Form("example %index abc") results in "example %s abc"
+   **/
+  public Form(String form) {
+    this(form.split(WORD_EDGE));
   }
 
   public String with(Object... insertables) {
     return String.format(format.toString(), insertables);
   }
-
   public String of(Object... insertables) {
     return with(insertables);
   }
